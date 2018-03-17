0111fa7beb108bb8e2db598467dfb3268cbcb4e9
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2018-03-17 21:26:02 +0000

    Add main menu

--- Practica2/main.c
+++ Practica2/main.c
@@ -1,33 +1,55 @@
 #include <stdio.h>
 #include "LS_allegro.h"
 
-int main(void){
-	
+int main () {
+	char opcio = ' ';
 	int nSortir = 0;
 	
-	//Inicialitzem Allegro
-	LS_allegro_init(1280,720,"PGM1 - PRS 2");
-	
-	
-	//Bucle infinit del joc
-	while(!nSortir){
+	while (opcio != '3') {
 		
-		//Escoltem el teclat esperant la tecla ESC
-		if(LS_allegro_key_pressed(ALLEGRO_KEY_ESCAPE)){
-			nSortir = 1;
+		printf ("Menu:\n");
+		printf ("1- Nova partida\n");
+		printf ("2- Mostrar ranquing\n");
+		printf ("3- Sortir\n");
+		
+		switch (opcio) {
+			case '1': // Nova partida
+			
+				//Inicialitzem Allegro
+				LS_allegro_init(1280,720,"PGM1 - PRS 2");
+	
+	
+				//Bucle infinit del joc
+				while(!nSortir){
+		
+					//Escoltem el teclat esperant la tecla ESC
+					if(LS_allegro_key_pressed(ALLEGRO_KEY_ESCAPE)){
+						nSortir = 1;
+					}
+					
+					//Donem l'ordre d'escriure el text de benvinguda
+					al_draw_textf(LS_allegro_get_font(NORMAL),LS_allegro_get_color(WHITE),140,100,0,"%s","Benvingut a Allegro! Prem ESC per sortir...");
+				
+					//Pintem la pantalla de la finestra gr‡fica
+					LS_allegro_clear_and_paint(BLACK);
+				}
+				
+				
+				//Tanquem la finestra gr‡fica
+				LS_allegro_exit();
+				break;
+			case '2': // Mostrar ranquing
+			
+				break;
+				
+			case '3': // Sortir
+				printf ("\nGracies per jugar!\n");
+				break;
+				
+			default: // En cas d'error en l'opcio
+				printf ("Error, opcio incorrecta. Ha de ser un numero d'1 a 3.\n");
+				break;
 		}
-		
-		//Donem l'ordre d'escriure el text de benvinguda
-		al_draw_textf(LS_allegro_get_font(NORMAL),LS_allegro_get_color(WHITE),380,100,0,"%s","Benvingut a Allegro! Prem ESC per sortir...");
-		//Pintem la pantalla de la finestra gr√†fica
-		LS_allegro_clear_and_paint(BLACK);
 	}
-	
-	
-	//Tanquem la finestra gr√†fica
-	LS_allegro_exit();
-	
-	
-	
 	return 0;
 }
