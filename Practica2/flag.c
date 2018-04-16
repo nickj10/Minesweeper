383f204d6da7e0b5bd767dfb6acebe99cc04626c
nickj10 <jas.jimenez411@gmail.com>
2018-04-16 12:16:20 +0200

    Finish FLAG module

--- Practica2/flag.c
+++ Practica2/flag.c
@@ -29,29 +29,54 @@ void FLAG_inserir (Flags *f, Elemento e) {
 }
 
 void FLAG_borrar (Flags *f) {
-
+    Nodo *aux;
+    if (f->ant->sig != NULL) {
+        aux = f->ant->sig;
+        f->ant->sig = aux->sig;
+        free (aux);
+    }
 }
 
 Elemento FLAG_consultar (Flags f) {
-
+    Elemento e;
+    if (f->ant->sig == NULL) {
+        e.col = -1;
+        e.fila = -1;
+    }
+    else {
+        e.col = f->ant->e.col;
+        e.fila = f->ant->e.fila;
+    }
+    return e;
 }
 
 int FLAG_vacia (Flags f) {
-
+    return f.pri->sig == NULL;
 }
 
 void FLAG_irInicio (Flags *f) {
-
+    f->ant = f->pri;
 }
 
 void FLAG_avanzar (Flags *f) {
-
+    if (f->ant->sig == NULL) {
+        printf ("Error en anvanzar en la lista.\n");
+    }
+    else {
+        f->ant = f->ant->sig;
+    }
 }
 
 int FLAG_final (Flags f) {
-
+    return f.ant->sig == NULL;
 }
 
 void FLAG_destruye (Flags *f) {
-
+    FLAG_irInicio(f);
+    while (!FLAG_vacia(*f)) {
+        FLAG_borrar(f);
+    }
+    free (f->pri);
+    f->pri = NULL;
+    f->ant = NULL;
 }
